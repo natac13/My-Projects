520d5a97c8777b5318cfdc9a0b1009f0a2075872
Natac13 <Natac13@user.noreply.github.com>
2015-02-12 17:20:32 -0500

    basic operation or flow of game is done. NEED to update the README better and clean up the code, too late atm

--- BlackJack/blackJack_newDECK.py
+++ BlackJack/blackJack_newDECK.py
@@ -34,13 +34,13 @@ def draw_card(deck):
     return new_cards
 
 
-def blackjackCalculator(userHand):
-    print('\nplayerhand', userHand, '\n')
+def blackjackCalculator(Hand):
+    
     
     ########   BLACK JACK CALCULATOR   #######
     total = 0
     ace = False
-    for card in userHand:
+    for card in Hand:
         try:
             total += card[0]
         except TypeError:
@@ -49,23 +49,26 @@ def blackjackCalculator(userHand):
             else:
                 total += 1
                 ace = True
-    print("Hand total", total)
+    
     hard = total
     soft = total
     if ace:
         hard += 10
         if hard <= 21:
-            print("hard total with ace", hard)
-            print("soft total with ace", soft)
-            print("HARD IS RETURNED")
+            # print("hard total with ace", hard)
+            # print("soft total with ace", soft)
+            # print("HARD IS RETURNED")
+            return hard
         else:
-            print("soft total with ace", soft)
-            print("hard total with ace", hard)
-            print("SOFT IS RETURNED")
+            # print("soft total with ace", soft)
+            # print("hard total with ace", hard)
+            # print("SOFT IS RETURNED")
+            return soft
+    return total ### this is if no ace
 
     
 main_deck = make_deck()
-print(main_deck)
+# print(main_deck)
 print("\n", len(main_deck))
 
 #print(main_deck)
@@ -73,20 +76,83 @@ while True:
     compHand, userHand = [], []
     for i in range(2):
         userHand += draw_card(main_deck)
-        
-
+        compHand += draw_card(main_deck)
     blackjackCalculator(userHand)
-   
-    input(">>>>>")
-    userHand += draw_card(main_deck)
-    blackjackCalculator(userHand)
-   
-    input(">>>>>")
-    userHand += draw_card(main_deck)
-    blackjackCalculator(userHand)
-   
-    input("LASTTIME   >>>>>")
-    userHand += draw_card(main_deck)
-    blackjackCalculator(userHand)
+    
+    go = True
+    while go:
+        print('\nplayerhand', userHand, '\n')
+        print("COMP SHOWING", compHand[0])
+        print(blackjackCalculator(userHand))
+   ######## A way to check for blackjack, but only the first time #####
+        # if blackjackCalculator(userHand) == 21:
+            # print("User WIN")
+            # print("COMP>>> {0}".format(blackjackCalculator(compHand)))
+            # print("USER>>> {0}".format(blackjackCalculator(userHand)))
+            # break
+        choice = input("H, S?>>>>>")
+        if choice == 'h':
+            userHand += draw_card(main_deck)
+            print(blackjackCalculator(userHand))
+        else:
+            print("COMP shows", compHand)
+            
+            ## if user hand bigger then 21 then just show comp hand and end turn
+            if blackjackCalculator(userHand) > 21:
+                print("COMP WIN")
+                print("COMP>>> {0}".format(blackjackCalculator(compHand)))
+                print("USER>>> {0}".format(blackjackCalculator(userHand)))
+                break
+            if blackjackCalculator(compHand) < 17:
+                compHand += draw_card(main_deck)
+                
+                print("COMP shows", compHand)
+                if blackjackCalculator(compHand) < 17:
+                    compHand += draw_card(main_deck)
+                    print("COMP shows", compHand)
+                    if blackjackCalculator(compHand) < 17:
+                        compHand += draw_card(main_deck)
+                        print("COMP shows", compHand)
+                    else:
+                        if blackjackCalculator(compHand) > blackjackCalculator(userHand) and blackjackCalculator(compHand) <= 21:
+                            print("COMP WIN")
+                            print("COMP>>> {0}".format(blackjackCalculator(compHand)))
+                            print("USER>>> {0}".format(blackjackCalculator(userHand)))
+                            break
+                        else:
+                            print("User WIN")
+                            print("COMP>>> {0}".format(blackjackCalculator(compHand)))
+                            print("USER>>> {0}".format(blackjackCalculator(userHand)))
+                            break    
+                else:
+                    if blackjackCalculator(compHand) > blackjackCalculator(userHand) and blackjackCalculator(compHand) <= 21:
+                        print("COMP WIN")
+                        print("COMP>>> {0}".format(blackjackCalculator(compHand)))
+                        print("USER>>> {0}".format(blackjackCalculator(userHand)))
+                        break
+                    else:
+                        print("User WIN")
+                        print("COMP>>> {0}".format(blackjackCalculator(compHand)))
+                        print("USER>>> {0}".format(blackjackCalculator(userHand)))
+                        break
+            else:
+                if blackjackCalculator(compHand) > blackjackCalculator(userHand) and blackjackCalculator(compHand) <= 21:
+                    print("COMP WIN")
+                    print("COMP>>> {0}".format(blackjackCalculator(compHand)))
+                    print("USER>>> {0}".format(blackjackCalculator(userHand)))
+                    break
+                else:
+                    print("User WIN")
+                    print("COMP>>> {0}".format(blackjackCalculator(compHand)))
+                    print("USER>>> {0}".format(blackjackCalculator(userHand)))
+                    break
+       
+        # input(">>>>>")
+        # userHand += draw_card(main_deck)
+        # blackjackCalculator(userHand)
+       
+        # input("LASTTIME   >>>>>")
+        # userHand += draw_card(main_deck)
+        # blackjackCalculator(userHand)
    
     
