4f4f309e592c7b481b55989885c93d27a1df9a39
Natac13 <Natac13@user.noreply.github.com>
2015-02-12 15:02:51 -0500

    got blackjack calculator working... treat all ace as one!

--- BlackJack/blackJack_newDECK.py
+++ BlackJack/blackJack_newDECK.py
@@ -1,15 +1,15 @@
-
+import random
 
 suits = ['Spade', 'Heart', 'Club', 'Diamond']
 values = list(range(2, 11)) + ['A', 'J', 'Q', 'K'] 
 
 def make_deck():
     '''Generate list of tuples, which are the card of a deck'''
-    deck = [(rank, suit) for rank in values for suit in suits]
+    deck = [(rank, suit) for rank in values for suit in suits] * 4
     random.shuffle(deck) # does not return anything....
     return deck
-
-def draw_card(n, deck):
+    
+def draw_card(deck):
     ''' 
     n: an integer which is how many card to draw from the deck, usually 1
     deck: is a non-empty list of tuples when n is 9
@@ -18,38 +18,71 @@ def draw_card(n, deck):
     will return none is the deck is empty
     '''
     new_cards = []
-    if n == 1:
-        try:
-            card = random.choice(deck)
-        except IndexError:
-            print("No cards, left in deck")
-            return []
-        else:
-            deck.remove(card)
-            new_cards.append(card)
-
+    
+    try:
+        card = deck.pop(0) # no need to del since pop() removes element as well
+    except IndexError:
+        print("No cards, left in deck")
+        return []
+    else:
+        new_cards.append(card)
+    
     return new_cards
+
+
+def blackjackCalculator(userHand):
+    print('\nplayerhand', userHand, '\n')
+    
+    ########   BLACK JACK CALCULATOR   #######
+    total = 0
+    ace = False
+    for card in userHand:
+        try:
+            total += card[0]
+        except TypeError:
+            if card[0] in 'JQK':
+                total += 10
+            else:
+                total += 1
+                ace = True
+    print("Hand total", total)
+    hard = total
+    soft = total
+    if ace:
+        hard += 10
+        if hard <= 21:
+            print("hard total with ace", hard)
+            print("soft total with ace", soft)
+            print("HARD IS RETURNED")
+        else:
+            print("soft total with ace", soft)
+            print("hard total with ace", hard)
+            print("SOFT IS RETURNED")
+
     
 main_deck = make_deck()
 print(main_deck)
+print("\n", len(main_deck))
 
 #print(main_deck)
-compHand, userHand = [], []
-for i in range(2):
-    compHand += draw_card(1, main_deck)
-    userHand += draw_card(1, main_deck)
+while True:
+    compHand, userHand = [], []
+    for i in range(2):
+        userHand += draw_card(main_deck)
+        
 
-print('playerhand', userHand, '\n')
-print('comphand', compHand)
-########   BLACK JACK CALCULATOR   #######
-total = 0
-for card in userHand:
-    try:
-        total += card[0]
-    except TypeError:
-        if card[0] in 'JQK':
-            total += 10
-        else:
-            total += 11
-print('\nplayerhand', userHand, '\n')
-print(total)
+    blackjackCalculator(userHand)
+   
+    input(">>>>>")
+    userHand += draw_card(main_deck)
+    blackjackCalculator(userHand)
+   
+    input(">>>>>")
+    userHand += draw_card(main_deck)
+    blackjackCalculator(userHand)
+   
+    input("LASTTIME   >>>>>")
+    userHand += draw_card(main_deck)
+    blackjackCalculator(userHand)
+   
+    
